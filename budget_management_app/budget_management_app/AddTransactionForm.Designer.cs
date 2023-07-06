9516f1837f409fde441959d164a1a1c598609afe
Maria <maria.shyliankova@gmail.com>
2023-07-06 19:26:26 +0200

    Design AddTransactionForm

--- budget_management_app/budget_management_app/AddTransactionForm.Designer.cs
+++ budget_management_app/budget_management_app/AddTransactionForm.Designer.cs
@@ -28,7 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_exit = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_currency = new System.Windows.Forms.Label();
+            this.textBox_amount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ComboBox_category = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ComboBox_type = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Button_add = new Guna.UI2.WinForms.Guna2TileButton();
+            this.ComboBox_account = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(213)))), ((int)(((byte)(174)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label_exit);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 55);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 29);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "New Transaction";
+            // 
+            // label_exit
+            // 
+            this.label_exit.AutoSize = true;
+            this.label_exit.Font = new System.Drawing.Font("Segoe UI Black", 19.25F, System.Drawing.FontStyle.Bold);
+            this.label_exit.ForeColor = System.Drawing.Color.White;
+            this.label_exit.Location = new System.Drawing.Point(550, 0);
+            this.label_exit.Name = "label_exit";
+            this.label_exit.Size = new System.Drawing.Size(33, 36);
+            this.label_exit.TabIndex = 36;
+            this.label_exit.Text = "X";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(213)))), ((int)(((byte)(174)))));
+            this.panel2.Location = new System.Drawing.Point(0, 695);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(583, 83);
+            this.panel2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Heading", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(10, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 28);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Value";
+            // 
+            // label_currency
+            // 
+            this.label_currency.AutoSize = true;
+            this.label_currency.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label_currency.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.label_currency.Location = new System.Drawing.Point(332, 73);
+            this.label_currency.Name = "label_currency";
+            this.label_currency.Size = new System.Drawing.Size(70, 37);
+            this.label_currency.TabIndex = 65;
+            this.label_currency.Text = "USD";
+            // 
+            // textBox_amount
+            // 
+            this.textBox_amount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_amount.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold);
+            this.textBox_amount.Location = new System.Drawing.Point(174, 71);
+            this.textBox_amount.Name = "textBox_amount";
+            this.textBox_amount.Size = new System.Drawing.Size(152, 40);
+            this.textBox_amount.TabIndex = 64;
+            this.textBox_amount.Text = "0";
+            this.textBox_amount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.label6.Location = new System.Drawing.Point(174, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 13);
+            this.label6.TabIndex = 63;
+            this.label6.Text = "________________________";
+            // 
+            // ComboBox_category
+            // 
+            this.ComboBox_category.BackColor = System.Drawing.Color.Transparent;
+            this.ComboBox_category.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.ComboBox_category.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComboBox_category.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_category.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_category.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_category.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.ComboBox_category.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.ComboBox_category.ItemHeight = 30;
+            this.ComboBox_category.Items.AddRange(new object[] {
+            "Income",
+            "Expense",
+            "Savings"});
+            this.ComboBox_category.Location = new System.Drawing.Point(10, 417);
+            this.ComboBox_category.Name = "ComboBox_category";
+            this.ComboBox_category.Size = new System.Drawing.Size(551, 36);
+            this.ComboBox_category.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Heading", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(10, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 28);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "Category";
+            // 
+            // ComboBox_type
+            // 
+            this.ComboBox_type.BackColor = System.Drawing.Color.Transparent;
+            this.ComboBox_type.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.ComboBox_type.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComboBox_type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_type.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_type.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_type.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.ComboBox_type.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.ComboBox_type.ItemHeight = 30;
+            this.ComboBox_type.Items.AddRange(new object[] {
+            "Income",
+            "Expense",
+            "Savings"});
+            this.ComboBox_type.Location = new System.Drawing.Point(10, 307);
+            this.ComboBox_type.Name = "ComboBox_type";
+            this.ComboBox_type.Size = new System.Drawing.Size(551, 36);
+            this.ComboBox_type.TabIndex = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Heading", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(10, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 28);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Type";
+            // 
+            // Button_add
+            // 
+            this.Button_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(237)))), ((int)(((byte)(205)))));
+            this.Button_add.BorderThickness = 1;
+            this.Button_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Button_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Button_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Button_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Button_add.FillColor = System.Drawing.Color.Transparent;
+            this.Button_add.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Button_add.ForeColor = System.Drawing.Color.Black;
+            this.Button_add.Location = new System.Drawing.Point(215, 491);
+            this.Button_add.Name = "Button_add";
+            this.Button_add.Size = new System.Drawing.Size(146, 36);
+            this.Button_add.TabIndex = 58;
+            this.Button_add.Text = "ADD";
+            // 
+            // ComboBox_account
+            // 
+            this.ComboBox_account.BackColor = System.Drawing.Color.Transparent;
+            this.ComboBox_account.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.ComboBox_account.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComboBox_account.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_account.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_account.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ComboBox_account.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.ComboBox_account.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.ComboBox_account.ItemHeight = 30;
+            this.ComboBox_account.Items.AddRange(new object[] {
+            "Income",
+            "Expense",
+            "Savings"});
+            this.ComboBox_account.Location = new System.Drawing.Point(10, 197);
+            this.ComboBox_account.Name = "ComboBox_account";
+            this.ComboBox_account.Size = new System.Drawing.Size(551, 36);
+            this.ComboBox_account.TabIndex = 57;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Heading", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(10, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 28);
+            this.label5.TabIndex = 56;
+            this.label5.Text = "Account";
             // 
             // AddTransactionForm
             // 
@@ -36,14 +245,46 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(580, 750);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_currency);
+            this.Controls.Add(this.textBox_amount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ComboBox_category);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ComboBox_type);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Button_add);
+            this.Controls.Add(this.ComboBox_account);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddTransactionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddTransactionForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_exit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_currency;
+        private System.Windows.Forms.TextBox textBox_amount;
+        private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2ComboBox ComboBox_category;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2ComboBox ComboBox_type;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TileButton Button_add;
+        private Guna.UI2.WinForms.Guna2ComboBox ComboBox_account;
+        private System.Windows.Forms.Label label5;
     }
 }
