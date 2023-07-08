d4cfa40a49979bd64cc8442a5449c6e109202e8f
Maria <maria.shyliankova@gmail.com>
2023-07-08 14:29:25 +0200

    Added PieChar for summary (currentMonth)

--- budget_management_app/budget_management_app/StartPageForm.Designer.cs
+++ budget_management_app/budget_management_app/StartPageForm.Designer.cs
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label_exit = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label_exit = new System.Windows.Forms.Label();
             this.flowLayoutPanel_account = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart_summary = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label_total = new System.Windows.Forms.Label();
+            this.Button_add = new Guna.UI2.WinForms.Guna2TileButton();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_summary)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -46,6 +54,28 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(583, 50);
             this.panel2.TabIndex = 30;
+            // 
+            // label_exit
+            // 
+            this.label_exit.AutoSize = true;
+            this.label_exit.Font = new System.Drawing.Font("Segoe UI Black", 19.25F, System.Drawing.FontStyle.Bold);
+            this.label_exit.ForeColor = System.Drawing.Color.White;
+            this.label_exit.Location = new System.Drawing.Point(550, 0);
+            this.label_exit.Name = "label_exit";
+            this.label_exit.Size = new System.Drawing.Size(33, 36);
+            this.label_exit.TabIndex = 37;
+            this.label_exit.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(250, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 29);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Home";
             // 
             // panel1
             // 
@@ -65,28 +95,6 @@
             this.label1.TabIndex = 32;
             this.label1.Text = "Accounts";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(250, 11);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 29);
-            this.label2.TabIndex = 36;
-            this.label2.Text = "Home";
-            // 
-            // label_exit
-            // 
-            this.label_exit.AutoSize = true;
-            this.label_exit.Font = new System.Drawing.Font("Segoe UI Black", 19.25F, System.Drawing.FontStyle.Bold);
-            this.label_exit.ForeColor = System.Drawing.Color.White;
-            this.label_exit.Location = new System.Drawing.Point(550, 0);
-            this.label_exit.Name = "label_exit";
-            this.label_exit.Size = new System.Drawing.Size(33, 36);
-            this.label_exit.TabIndex = 37;
-            this.label_exit.Text = "X";
-            // 
             // flowLayoutPanel_account
             // 
             this.flowLayoutPanel_account.AutoScroll = true;
@@ -97,12 +105,70 @@
             this.flowLayoutPanel_account.Size = new System.Drawing.Size(561, 75);
             this.flowLayoutPanel_account.TabIndex = 33;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Small Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(3, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 28);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Summary";
+            // 
+            // chart_summary
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart_summary.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart_summary.Legends.Add(legend2);
+            this.chart_summary.Location = new System.Drawing.Point(8, 218);
+            this.chart_summary.Name = "chart_summary";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            series2.Legend = "Legend1";
+            series2.Name = "DataSeries";
+            this.chart_summary.Series.Add(series2);
+            this.chart_summary.Size = new System.Drawing.Size(249, 169);
+            this.chart_summary.TabIndex = 35;
+            // 
+            // label_total
+            // 
+            this.label_total.AutoSize = true;
+            this.label_total.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_total.Location = new System.Drawing.Point(194, 326);
+            this.label_total.Name = "label_total";
+            this.label_total.Size = new System.Drawing.Size(47, 17);
+            this.label_total.TabIndex = 36;
+            this.label_total.Text = "Total : ";
+            // 
+            // Button_add
+            // 
+            this.Button_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(237)))), ((int)(((byte)(205)))));
+            this.Button_add.BorderThickness = 1;
+            this.Button_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Button_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Button_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Button_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Button_add.FillColor = System.Drawing.Color.Transparent;
+            this.Button_add.Font = new System.Drawing.Font("Segoe UI Semibold", 10.75F, System.Drawing.FontStyle.Bold);
+            this.Button_add.ForeColor = System.Drawing.Color.Black;
+            this.Button_add.Location = new System.Drawing.Point(8, 402);
+            this.Button_add.Name = "Button_add";
+            this.Button_add.Size = new System.Drawing.Size(119, 25);
+            this.Button_add.TabIndex = 59;
+            this.Button_add.Text = "Show More";
+            // 
             // StartPageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(581, 750);
+            this.Controls.Add(this.Button_add);
+            this.Controls.Add(this.label_total);
+            this.Controls.Add(this.chart_summary);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.flowLayoutPanel_account);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
@@ -114,6 +180,7 @@
             this.Load += new System.EventHandler(this.StartPageForm_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_summary)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,5 +194,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label_exit;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_account;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_summary;
+        private System.Windows.Forms.Label label_total;
+        private Guna.UI2.WinForms.Guna2TileButton Button_add;
     }
 }
--- budget_management_app/budget_management_app/StartPageForm.cs
+++ budget_management_app/budget_management_app/StartPageForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace budget_management_app
 {
@@ -22,9 +23,10 @@ namespace budget_management_app
 
         private void StartPageForm_Load(object sender, EventArgs e)
         {
-            getTable();
+            getTableAcc();
+            getDataSum();
         }
-        private void getTable()
+        private void getTableAcc()
         {
             string selectQuerry = "SELECT AccName,AccBalance,Currency.CurrCode FROM Account JOIN Currency ON Account.AccCurrId = Currency.CurrId WHERE UserId=" + LoginForm.userId;
             SqlCommand command = new SqlCommand(selectQuerry, dbcon.GetCon());
@@ -47,6 +49,34 @@ namespace budget_management_app
                 flowLayoutPanel_account.Controls.Add(button);
             }
             dbcon.CloseCon();
+        }
+        private void getDataSum()
+        {
+            DateTime currentDate=DateTime.Now;
+            int currentMonth=currentDate.Month;
+            int currentYear=currentDate.Year;
+
+            string selectExpensesQuery = "SELECT SUM(ExpAmount) AS TotalExpenses FROM Expenses WHERE UserId=" + LoginForm.userId + " AND MONTH(ExpDate) = " + currentMonth + " AND YEAR(ExpDate) = " + currentYear;
+            SqlCommand expensesCommand = new SqlCommand(selectExpensesQuery, dbcon.GetCon());
+            decimal totalExpenses = Convert.ToDecimal(expensesCommand.ExecuteScalar());
+
+            string selectIncomeQuery = "SELECT SUM(InAmount) AS TotalIncome FROM Income WHERE UserId=" + LoginForm.userId + " AND MONTH(InDate) = " + currentMonth + " AND YEAR(InDate) = " + currentYear;
+            SqlCommand incomeCommand = new SqlCommand(selectIncomeQuery, dbcon.GetCon());
+            decimal totalIncome = Convert.ToDecimal(incomeCommand.ExecuteScalar());
+
+            string selectSavingsQuery = "SELECT SUM(SavAmount) AS TotalSavings FROM Savings WHERE UserId=" + LoginForm.userId + " AND MONTH(SavDate) = " + currentMonth + " AND YEAR(SavDate) = " + currentYear;
+            SqlCommand savingsCommand = new SqlCommand(selectSavingsQuery, dbcon.GetCon());
+            decimal totalSavings = Convert.ToDecimal(savingsCommand.ExecuteScalar());
+
+            decimal totalAmount = totalExpenses + totalIncome + totalSavings;
+
+            decimal expensesPercentage = (totalExpenses / totalAmount) * 100;
+            decimal incomePercentage = (totalIncome / totalAmount) * 100;
+            decimal savingsPercentage = (totalSavings / totalAmount) * 100;
+
+            chart_summary.Series["DataSeries"].Points.AddXY("Expenses", expensesPercentage);
+            chart_summary.Series["DataSeries"].Points.AddXY("Income", incomePercentage);
+            chart_summary.Series["DataSeries"].Points.AddXY("Savings", savingsPercentage);
         }
     }
 }
